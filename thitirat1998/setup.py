998d2dbb0978771ad86de2f1400603f8d0247455
sinsopa6566 <thitirat.si.59@ubu.ac.th>
2017-11-17 16:19:38 +0700

    add thitirat1998 project

---
+++ thitirat1998/setup.py
@@ -0,0 +1,48 @@
+"""
+My Tool does one thing, and one thing well.
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='thitirat1998',
+    version='0.1.0',
+    url='https://github.com/thitirat1998/thitirat1998',
+    license='BSD',
+    author='thitirat',
+    author_email='thitirat.si.59@ubu.ac.th',
+    description='My Tool does one thing, and one thing well.',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'thitirat1998 = thitirat1998.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
