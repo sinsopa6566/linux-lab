bb9060656b2ea7eb5deea59845ef2a6ffd591af3
sinsopa6566 <thitirat.si.59@ubu.ac.th>
2017-12-05 03:16:52 +0700

    linux-cli

--- thitirat1998/thitirat1998/cli.py
+++ thitirat1998/thitirat1998/cli.py
@@ -1,10 +1,30 @@
 import click
+import requests
 
+from bs4 import BeautifulSoup
+from PIL import Image
+from StringIO import StringIO
 
 @click.command()
-@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
-@click.argument('name', default='world', required=False)
-def main(name, as_cowboy):
-    """My Tool does one thing, and one thing well."""
-    greet = 'Howdy' if as_cowboy else 'Hello'
-    click.echo('{0}, {1}.'.format(greet, name))
+@click.argument('src', nargs=-1)
+@click.argument('dst', nargs=1)
+def main(src, dst):
+    	"""Get Photo Phone From Siamphone"""
+	src_search=""	
+	for fn in src:
+       		src_search+='%s' % (fn)+" "
+	src_search+='%s'%(dst)
+	url='https://www.google.co.in/search?q='+src_search+' siamphone'
+	html=requests.get(url.replace(' ','+'))
+	b=BeautifulSoup(html.content,'html.parser')
+	getUrl=b.find_all('h3',{'class':'r'})[0].a['href'].replace('/url?q=','')
+	for x in range(len(str(getUrl))):
+		if getUrl[x]=='&':
+			getUrl=getUrl[:x]
+			break
+	html=requests.get(getUrl)
+	b=BeautifulSoup(html.content,'html.parser')
+	photo=b.find_all('div',{'id':'section_left'})[0].img['src']
+	req=requests.get('http://www.siamphone.com'+photo)
+	img=Image.open(StringIO(req.content))
+	img.show()
